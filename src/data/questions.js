459f7df307d2efad3b610a3ae8087c046657fb96
dharshinir11 <166281402+dharshinir11@users.noreply.github.com>
2025-08-05 10:33:58 +0530

    Update questions.js

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,6 +1,26 @@
 export default {
-  Q1: "Was the session engaging?",
-  Q2: "Was the session helpful to your understanding?",
-  Q3: "Any suggestions for improvement?",
-  Q4: "Rate the session"
+  Q1: "Opinion about overall session",
+  Q2: "Clarity in the speech",
+  Q3: "Speaker's interaction with the students",
+  Q4: "Did the session deliver the information you expected to receive",
+  Q5: "Was the subject matter presented effectively",
+  Q6: "Is the speaker knowledgeable",
+  Q7: "Suggestions/Questions/Feedback"
+
+  "Keeladi - Local Visit questions"
+  "1. How would you rate your overall learning experience during the Keeladi visit?",
+  "2. Did the trip enhance your understanding of ancient Tamil civilization and heritage?",
+  "3. Was the explanation at the site informative and engaging?",
+  "4. Were the travel and logistical arrangements adequate and comfortable?",
+  "5. Suggestions/Questions/Feedback"
+
+
+  "Diagnostic - Test feedback"
+  "1. Was the test content aligned with your academic level?",
+  "2. Were the instructions for the test clear and easy to follow?",
+  "3. Was the allotted time sufficient to complete the test?",
+  "4. Did the test help you understand your strengths and areas to improve?",
+  "5. Suggestions/Questions/Feedback"
 };
+
+
