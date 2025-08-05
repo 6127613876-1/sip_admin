be2957d168c787d2d0bd3363ac82b4bb2d5a1717
dharshinir11 <166281402+dharshinir11@users.noreply.github.com>
2025-08-05 10:38:41 +0530

    Update questions.js

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,26 +1,29 @@
 export default {
-  Q1: "Opinion about overall session",
-  Q2: "Clarity in the speech",
-  Q3: "Speaker's interaction with the students",
-  Q4: "Did the session deliver the information you expected to receive",
-  Q5: "Was the subject matter presented effectively",
-  Q6: "Is the speaker knowledgeable",
-  Q7: "Suggestions/Questions/Feedback"
+  sessionFeedback: {
+    Q1: "Opinion about overall session",
+    Q2: "Clarity in the speech",
+    Q3: "Speaker's interaction with the students",
+    Q4: "Did the session deliver the information you expected to receive",
+    Q5: "Was the subject matter presented effectively",
+    Q6: "Is the speaker knowledgeable",
+    Q7: "Suggestions/Questions/Feedback"
+  },
 
-  "Keeladi - Local Visit questions"
-  "1. How would you rate your overall learning experience during the Keeladi visit?",
-  "2. Did the trip enhance your understanding of ancient Tamil civilization and heritage?",
-  "3. Was the explanation at the site informative and engaging?",
-  "4. Were the travel and logistical arrangements adequate and comfortable?",
-  "5. Suggestions/Questions/Feedback"
+  keeladiVisitFeedback: {
+    Q1: "How would you rate your overall learning experience during the Keeladi visit?",
+    Q2: "Did the trip enhance your understanding of ancient Tamil civilization and heritage?",
+    Q3: "Was the explanation at the site informative and engaging?",
+    Q4: "Were the travel and logistical arrangements adequate and comfortable?",
+    Q5: "Suggestions/Questions/Feedback"
+  },
 
-
-  "Diagnostic - Test feedback"
-  "1. Was the test content aligned with your academic level?",
-  "2. Were the instructions for the test clear and easy to follow?",
-  "3. Was the allotted time sufficient to complete the test?",
-  "4. Did the test help you understand your strengths and areas to improve?",
-  "5. Suggestions/Questions/Feedback"
+  diagnosticTestFeedback: {
+    Q1: "Was the test content aligned with your academic level?",
+    Q2: "Were the instructions for the test clear and easy to follow?",
+    Q3: "Was the allotted time sufficient to complete the test?",
+    Q4: "Did the test help you understand your strengths and areas to improve?",
+    Q5: "Suggestions/Questions/Feedback"
+  }
 };
 
 
