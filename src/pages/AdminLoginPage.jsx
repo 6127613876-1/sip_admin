3d8f0dcebfbe3efbe5b803da2f3d4c73aa7b12d8
Ridhyadharshinni <ridhyatr@gmail.com>
2025-08-05 09:29:47 +0530

    Update AdminLoginPage.jsx

--- src/pages/AdminLoginPage.jsx
+++ src/pages/AdminLoginPage.jsx
@@ -21,7 +21,7 @@ export const AdminLoginPage = () => {
     return (
         <div className="min-h-screen  flex flex-col items-center justify-center p-4" >
             <img src="images/college_logo.png" alt="College Logo" className="w-20 max-w-md h-auto mb-6 rounded-lg shadow-lg" />     
-            <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">First Year SIP-Feedback Portal</h1>
+            <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">First Year Student Induction Programme-Feedback Portal</h1>
             <div className="w-full max-w-sm bg-white rounded-lg shadow-xl p-8">
                 <h2 className="text-2xl font-bold text-center mb-6 text-gray-700">Admin Login</h2>
                 {error && <p className="text-red-500 text-center mb-4">{error}</p>}
