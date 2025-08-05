6fba859c14a9c09ba8dbcd11dcf25f655f615277
dharshinir11 <166281402+dharshinir11@users.noreply.github.com>
2025-08-05 12:26:08 +0530

    Update feedbackApi.js

--- src/api/feedbackApi.js
+++ src/api/feedbackApi.js
@@ -4,14 +4,41 @@ import sessionData from '../data/sessionData'; // Adjust path if needed
 const API_URL = 'https://sip-1-uple.onrender.com/';
 
 export const questions = {
-  Q1: "Opinion about overall session",
-  Q2: "Clarity in the speech",
-  Q3: "Speaker's interaction with the students",
-  Q4: "Did the session deliver the information you expected to receive",
-  Q5: "Was the subject matter presented effectively",
-  Q6: "Is the speaker knowledgeable",
-  Q7: "Suggestions/Questions/Feedback"
+  Feedback: {
+    Q1: "Opinion about overall session",
+    Q2: "Clarity in the speech",
+    Q3: "Speaker's interaction with the students",
+    Q4: "Did the session deliver the information you expected to receive?",
+    Q5: "Was the subject matter presented effectively?",
+    Q6: "Is the speaker knowledgeable?",
+    Q7: "Suggestions/Questions/Feedback"
+  },
+
+  keeladiVisitFeedback: {
+    Q1: "How would you rate your overall learning experience during the Keeladi visit?",
+    Q2: "Did the trip enhance your understanding of ancient Tamil civilization and heritage?",
+    Q3: "Was the explanation at the site informative and engaging?",
+    Q4: "Were the travel and logistical arrangements adequate and comfortable?",
+    Q5: "Suggestions/Questions/Feedback"
+  },
+
+  diagnosticTestFeedback: {
+    Q1: "Was the test content aligned with your academic level?",
+    Q2: "Were the instructions for the test clear and easy to follow?",
+    Q3: "Was the allotted time sufficient to complete the test?",
+    Q4: "Did the test help you understand your strengths and areas to improve?",
+    Q5: "Suggestions/Questions/Feedback"
+  },
+
+  FinalFeedbackques: {
+        Q1:"To what extent did the induction program help you feel confident, informed, and prepared to begin your academic journey at our institution?",
+        Q2:"How would you rate the overall coordination and organization of the SIP program?",
+        Q3:"Were the basic amenities (seating, refreshments, transport, etc.) adequate throughout the program?",
+        Q4:"Was the sound system, session timing, and venue management handled effectively?",
+        Q5:"Please share your overall perspective on the SIP experience in a few sentences and which three sessions or activities did you find most impactful or engaging"                                                                                                                                                                }
+      };
 };
+
 
 /**
  * Helper to get sorted dates for a department from sessionData.
